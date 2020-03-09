ce07aa378c2a7d26b35188f84e05596f70843912
rodrigolazarte <romalaz94@gmail.com>
2020-03-09 14:10:53 -0300

    modificada vista parcial de lista de productos

--- viabana_nuevo/Controllers/CompraController.cs
+++ viabana_nuevo/Controllers/CompraController.cs
@@ -41,12 +41,39 @@ namespace viabana_nuevo.Controllers
         public ActionResult NuevaCompra()
         {
             List<string> Proveedores = new List<string>();
-            Proveedores.Add("Proveedor 1");
-            Proveedores.Add("Proveedor 2");
+            Proveedores.Add("Proveedor1");
+            Proveedores.Add("Proveedor2");
 
             ViewBag.Proveedores = Proveedores;
 
             return PartialView("Partials/_NuevaCompra");
+        }
+
+        public ActionResult ListaDeProductos(string index)
+        {
+            List<Producto> ListaProductos = new List<Producto>();
+            var producto1 = new Producto();
+            var producto2 = new Producto();
+
+            producto1.Descripcion = "Producto 1";
+            producto1.Id = 1;
+            producto1.Precio = 90;
+            producto1.Proveedor = "Proveedor1";
+
+            producto2.Descripcion = "Producto 2";
+            producto2.Id = 2;
+            producto2.Precio = 190;
+            producto2.Proveedor = "Proveedor2";
+
+            ListaProductos.Add(producto1);
+            ListaProductos.Add(producto2);
+
+            List<Producto> listaFiltrada = ListaProductos.Where(x => x.Proveedor.Equals(index)).ToList();
+
+            ViewBag.Productos = listaFiltrada;
+
+            return PartialView("Partials/_ListaProductosDeProveedor");
+
         }
     }
 }
---
+++ viabana_nuevo/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace viabana_nuevo.Models
+{
+    public class Producto
+    {
+        public string Descripcion { get; set; }
+        public float Precio { get; set; }
+        public int Id { get; set; }
+        public string Proveedor { get; set; }
+
+    }
+}
